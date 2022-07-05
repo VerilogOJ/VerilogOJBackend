271d963b6b5134fb9931d33553c69c56025f8c46
yxj <564197835@qq.com>
2022-07-05 17:38:42 +0800

    update frontend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,5 @@
 // vue.config.js
 module.exports = {
-  // In VLab environment, we use vlab.ustc.edu.cn/oj/... to access
   publicPath: process.env.NODE_ENV === "production" ? "/oj/" : "/",
 
   assetsDir: "",
